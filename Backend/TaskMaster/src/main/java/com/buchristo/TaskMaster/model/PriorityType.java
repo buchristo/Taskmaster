15fa53635706e204dab3feeae0a588f95a16f4c0
Christopher Bucher <bucher.christopher@gmail.com>
2023-10-30 11:27:03 +0100

    created entities and priority modelclass for taskmanager app

---
+++ Backend/TaskMaster/src/main/java/com/buchristo/TaskMaster/model/PriorityType.java
@@ -0,0 +1,5 @@
+package com.buchristo.TaskMaster.model;
+
+public enum PriorityType {
+    HIGH, MEDIUM, LOW
+}
---
+++ Backend/TaskMaster/src/main/java/com/buchristo/TaskMaster/persistence/Todo.java
@@ -0,0 +1,61 @@
+package com.buchristo.TaskMaster.persistence;
+
+import com.buchristo.TaskMaster.model.PriorityType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.Set;
+
+@Entity
+public class Todo {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private String description;
+    private PriorityType priorityType;
+
+    public Todo(Long id, String title, String description, PriorityType priorityType) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.priorityType = priorityType;
+    }
+
+    public Todo() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public PriorityType getPriorityType() {
+        return priorityType;
+    }
+
+    public void setPriorityType(PriorityType priorityType) {
+        this.priorityType = priorityType;
+    }
+}
